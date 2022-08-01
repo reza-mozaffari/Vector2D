e3327dcf0e291398ccee711ed3b415d30f12f362
reza-mozaffari <reza.mozaffari@gmail.com>
2022-08-01 09:11:48 +0430

    Add files via upload

---
+++ Vector2D.py
@@ -0,0 +1,41 @@
+from math import *
+
+class Vector2D ():
+	def __init__ (self, x=0.0, y=0.0):
+		self.x = float(x)
+		self.y = float(y)
+	
+	def __str__ (self):
+		return "({:.8f}, {:.8f})".format(self.x, self.y)
+			
+	def __add__ (self, other):
+		return Vector2D (self.x + other.x, self.y + other.y)
+		
+	def __sub__ (self, other):
+		return Vector2D (self.x - other.x, self.y - other.y)	
+	
+	def __mul__ (self, scalar):
+		return 	Vector2D (self.x*scalar, self.y*scalar)
+	
+	def __rmul__ (self, scalar):
+		return 	self.__mul__(scalar)
+	
+	def __div__ (self, scalar):
+		return Vector2D (self.x/float(scalar), self.y/float(scalar))
+	
+	def __truediv__ (self, scalar):
+		return self.__div__(scalar)
+				
+	def __abs__ (self):
+		return sqrt(self.x**2 + self.y**2)	
+
+	def dot (self, other):
+		return self.x*other.x + self.y*other.y
+
+	def cross (self, other):
+		return self.x*other.y - self.y*other.x
+
+	def hat (self):
+		r = self.__abs__()
+		return Vector2D (self.x/r, self.y/r)
+
---
+++ main.py
@@ -0,0 +1,15 @@
+
+from Vector2D import *
+
+if __name__ == "__main__":
+
+	V1 = Vector2D()
+	print("V1 = ", V1)
+
+	V2 = Vector2D(1.0, 2.0)
+	print("V2 = ", V2)
+
+	V1 = V2
+	print("V1 = ", V1)
+
+	print("3.0*V1/2.0 = ", 3.0*V1/2.0)
